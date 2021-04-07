4e28f5eeef87e5b657e3a41b7bedb368253472f5
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2021-04-06 20:30:09 -0400

    draw stuff on canvas

--- web-viewer/src/routes/home/index.tsx
+++ web-viewer/src/routes/home/index.tsx
@@ -1,23 +1,29 @@
 import { FunctionalComponent, h } from "preact";
+import { useEffect, useRef } from "preact/hooks";
+import Paho, { Message, ConnectionOptions } from "paho-mqtt";
 import style from "./style.css";
-import Paho, { Message } from "paho-mqtt";
 
 // useful commands:
 // mosquitto_pub -t cs326/jcalvin -m "Hello World" -h mqtt.eclipseprojects.io
 // mosquitto_sub -h mqtt.eclipseprojects.io -t cs326/jcalvin
+// mosquitto_sub -h iot.cs.calvin.edu -t pup/ctrl1 -u cs326 -P piot
+// https://medium.com/@pdx.lucasm/canvas-with-react-js-32e133c05258
 
 const Home: FunctionalComponent = () => {
-  const client = new Paho.Client("mqtt.eclipseprojects.io/mqtt", 80, "clientjs");
-  var options = {
+  const client = new Paho.Client("mqtt.eclipseprojects.io", 80, "clientjs");
+
+  const onConnect = () => {
+    console.log("Connected!");
+    client.subscribe("pup/ctrl1");
+  };
+  const options: ConnectionOptions = {
     useSSL: false,
     keepAliveInterval: 60,
     onSuccess: onConnect,
+    // userName: "cs326",
+    // password: "piot"
   };
   client.connect(options);
-  function onConnect() {
-    console.log("Connected!");
-    client.subscribe("cs326/jcalvin");
-  }
 
   const onMessageArrived = (message: Message) => {
     console.log("Message Arrived:" + message.payloadString);
@@ -25,12 +31,39 @@ const Home: FunctionalComponent = () => {
   };
   client.onMessageArrived = onMessageArrived;
 
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const draw = (ctx: CanvasRenderingContext2D) => {
+    ctx.fillStyle = "#000000";
+    ctx.beginPath();
+    ctx.arc(50, 100, 20, 0, 2 * Math.PI);
+    ctx.fill();
+  };
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    //Our first draw
+    if (context) {
+      context.fillStyle = "#000000";
+      context.fillRect(10, 10, 50, 50);
+      draw(context);
+    }
+  }, [draw]);
+
   return (
     <div class={style.home}>
       <h1>Home</h1>
       <p>This is the Home component.</p>
       <p>Hello there</p>
       <p id="msg"></p>
+      <canvas
+        ref={canvasRef}
+        id="myCanvas"
+        height={500}
+        width={500}
+        style={{ border: "1px solid green" }}
+      />
     </div>
   );
 };
